28ba09f1ddf4643f41b281bb0d945f6efcbeab90
jarqprog <jarqprog@gmail.com>
2018-04-25 09:26:37 +0200

    implement int[] version (standard)

--- src/main/java/com/codecool/dynamicArrayDojo/IDynamicIntArray.java
+++ src/main/java/com/codecool/dynamicArrayDojo/IDynamicIntArray.java
@@ -3,7 +3,7 @@ package com.codecool.dynamicArrayDojo;
 public interface IDynamicIntArray {
 
     void add(int num);
-    void remove(int num);
+    void remove(int index);
     void insert(int index, int num);
 
 }
